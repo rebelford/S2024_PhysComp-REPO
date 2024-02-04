67ef20345a4ef50ecd1e4a28d280364988bd2e3f
rebelford <rebelford@ualr.edu>
2024-02-04 16:36:43 -0600

    Create find_IP_address.py

---
+++ 7-Getting-started-with-Raspi/find_IP_address.py
@@ -0,0 +1,12 @@
+import socket
+#import fcntl
+import struct
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # uncomment the following line if using python2
+    #return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s',ifname[:15]))[20:24])
+    # uncomment the following line if using python3
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s',bytes(ifname[:15],'utf-8')))[20:24])    
+address = get_ip_address('wlan0')
+#change wlan0 to eth0 if you want the wired IP address
+print(address)
